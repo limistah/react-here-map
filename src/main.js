6c6441488a04856414a168f751e3faca197a06ca
Aleem Isiaka <aleemisiaka@gmail.com>
2019-01-13 12:27:20 +0100

    Init Router

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import HMap from "./components/HMap";
-import PolyLine from "./components/objects/HMap/PolyLine";
-import Polygon from "./components/objects/HMap/Polygon";
-import Marker from "./components/objects/HMap/Marker";
-import Circle from "./components/objects/HMap/Circle";
-import Rectangle from "./components/objects/HMap/Rectangle";
-import HMapGeoCode from "./components/HMapGeoCode";
+import PolyLine from "./components/HMap/objects/PolyLine";
+import Polygon from "./components/HMap/objects/Polygon";
+import Marker from "./components/HMap/objects/Marker";
+import Circle from "./components/HMap/objects/Circle";
+import Rectangle from "./components/HMap/objects/Rectangle";
+import GeoCode from "./components/GeoCode";
+import Route from "./components/Route";
 
 export const HMapPolyLine = PolyLine;
 export const HMapPolygon = Polygon;
 export const HMapMarker = Marker;
 export const HMapCircle = Circle;
 export const HMapRectangle = Rectangle;
+export const HMapRoute = Route;
 export const HMapGeoCode = GeoCode;
 export default HMap;
