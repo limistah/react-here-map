9f14bcdc4d35ea8fdef72a297b467d3e5667ce27
Aleem Isiaka <aleemisiaka@gmail.com>
2019-01-13 10:32:24 +0100

    Completes GeoCoding

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import Polygon from "./components/objects/Polygon";
 import Marker from "./components/objects/Marker";
 import Circle from "./components/objects/Circle";
 import Rectangle from "./components/objects/Rectangle";
+import HMapGeoCode from "./components/HMapGeoCode";
 
 export const HMapPolyLine = PolyLine;
 export const HMapPolygon = Polygon;
 export const HMapMarker = Marker;
 export const HMapCircle = Circle;
 export const HMapRectangle = Rectangle;
+export const HMapGeoCode = GeoCode;
 export default HMap;
